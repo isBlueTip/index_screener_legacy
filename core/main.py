4c63275f7653dbe79cb0085cb2349e929bc6bc2d
Simon Egorov <rhinorofl@gmail.com>
2023-07-24 17:51:00 +0700

    Format 'main.py'

--- core/main.py
+++ core/main.py
@@ -1,38 +1,39 @@
-from celery_.celery import app
-from celery_.tasks import hello
-from core.tasks import request_quote
 import pandas as pd
 
-STOCKS = pd.read_csv('../sp_500_stocks.csv')
+from celery_.celery import app
+from celery_.tasks import hello
+from core.tasks import request_cap, request_price
+
+# STOCKS = pd.read_csv('../sp_500_stocks.csv')
+STOCKS = pd.read_csv("/home/bluetip/dev/pet/sandp_screener/sp_500_stocks.csv")
 
 
 def request_quotes():
-    my_columns = ['Ticker', 'Stock Price', 'Market Capitalization', 'Number of Shares to Buy']
+    my_columns = ["Ticker", "Stock Price", "Market Capitalization", "Number of Shares to Buy"]
     final_dataframe = pd.DataFrame(columns=my_columns)
 
-    for symbol in STOCKS:
-        price, market_cap = request_quote.apply_async(symbol)
+    for symbol in STOCKS["Ticker"][:1]:
+        price = request_price.delay(symbol).get()
+        market_cap = request_cap.delay(symbol).get()
 
         print(f"price = {price}")
         print(f"market_cap = {market_cap / 1000000000000}")
 
-        final_dataframe += pd.concat([final_dataframe,
-                   pd.DataFrame(
-                       [
-                           [
-                               symbol,
-                               price,
-                               market_cap,
-                               'N/A'
-                           ],
-                       ],
-                       columns=my_columns
-                   )
-                  ]
-                 )
+        final_dataframe += pd.concat(
+            [
+                final_dataframe,
+                pd.DataFrame(
+                    [
+                        [symbol, price, market_cap, "N/A"],
+                    ],
+                    columns=my_columns,
+                ),
+            ]
+        )
+        print(f"final_dataframe = {final_dataframe}")
     print(final_dataframe)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # app.start()
-    request_quotes.delay()
+    request_quotes()
