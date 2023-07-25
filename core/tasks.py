d15dc627e1392028de13fb442a6f5555713d19b9
Simon Egorov <rhinorofl@gmail.com>
2023-07-25 20:33:19 +0700

    Update tasks

--- core/tasks.py
+++ core/tasks.py
@@ -2,7 +2,6 @@ from celery import Celery, shared_task
 from celery_.celery import app
 from utils.urls import get_quote_url, get_company_overview_url
 import requests
-import pandas as pd
 
 
 # @app.on_after_configure
@@ -15,24 +14,33 @@ import pandas as pd
 #     # defined to avoid this task replacing the previous one defined.
 #     # sender.add_periodic_task(30.0, test.s('hello'), name='add every 30')
 
-
-@shared_task
-# @app.task
+@app.task(rate_limit='10/m')
 def print_hello() -> str:
     return 'hello world'
 
 
-# @shared_task
-@app.task(rate_limit=10)
-def request_quote(symbol: str) -> (float, float):
-
+@app.task(rate_limit='2/m')
+def request_price(symbol: str) -> float:
     quote_url = get_quote_url(symbol)
-    company_overview_url = get_company_overview_url(symbol)
 
     data_price = requests.get(quote_url).json()
-    data_cap = requests.get(company_overview_url).json()
+
+    print(f'price data = {data_price}')
+    print(f'\n')
 
     price = float(data_price.get('Global Quote').get('05. price'))
+
+    return price
+
+
+@app.task(rate_limit='2/m')
+def request_cap(symbol: str) -> float:
+    company_overview_url = get_company_overview_url(symbol)
+
+    data_cap = requests.get(company_overview_url).json()
+    print(f'cap data = {data_cap}')
+    print(f'\n')
+
     market_cap = float(data_cap.get('MarketCapitalization'))
 
-    return price, market_cap
+    return market_cap
