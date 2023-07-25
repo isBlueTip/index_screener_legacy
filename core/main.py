3df0a9db632eebc5cc42dd159201e0a644ad558b
Simon Egorov <rhinorofl@gmail.com>
2023-07-25 20:32:04 +0700

    Concat pd.DataFrame instead of pd.Series

--- core/main.py
+++ core/main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-from celery_.celery import app
 from core.tasks import request_cap, request_price, print_hello
 
 # STOCKS = pd.read_csv('../sp_500_stocks.csv')
@@ -11,20 +10,19 @@ def request_quotes():
     my_columns = ["Ticker", "Stock Price", "Market Capitalization", "Number of Shares to Buy"]
     final_dataframe = pd.DataFrame(columns=my_columns)
 
-    for symbol in STOCKS["Ticker"][:4]:
+    for symbol in STOCKS["Ticker"][:3]:
         price = request_price.delay(symbol).get()
         market_cap = request_cap.delay(symbol).get() / 1000000000000
+
+        df = pd.DataFrame([[symbol, price, market_cap, "N/A"], ], columns=my_columns,)
 
         final_dataframe = pd.concat(
             [
                 final_dataframe,
-                pd.DataFrame(
-                    [
-                        [symbol, price, market_cap, "N/A"],
-                    ],
-                    columns=my_columns,
-                ),
-            ]
+                df,
+            ],
+            ignore_index=True,
+            axis=0,
         )
     return final_dataframe
 
